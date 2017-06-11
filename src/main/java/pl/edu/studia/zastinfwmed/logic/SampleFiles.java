8914b6f9e7b087109e78b68482c85378f8ea454d
Alicjaaa <alicja.majewska94@o2.pl>
2017-06-11 15:56:26 +0200

    More data files

--- src/main/java/pl/edu/studia/zastinfwmed/logic/SampleFiles.java
+++ src/main/java/pl/edu/studia/zastinfwmed/logic/SampleFiles.java
@@ -4,7 +4,10 @@ package pl.edu.studia.zastinfwmed.logic;
  * Created by Alicja on 2017-06-10.
  */
 public enum SampleFiles {
-    SAMPLE1("samples.csv"), SAMPLE2("samples_2.csv"), SAMPLE3("samples_3.csv"), SAMPLE4("samples_4.csv"), SAMPLE5("samples_5.csv");
+    SAMPLE1("samples.csv"), SAMPLE2("samples_2.csv"), SAMPLE3("samples_3.csv"), SAMPLE4("samples_4.csv"), SAMPLE5("samples_5.csv"),
+    SAMPLE6("samples_1_min.csv"), SAMPLE7("samples_2_min.csv"), SAMPLE8("samples_3_min.csv"), SAMPLE9("samples_4_min.csv"),
+    SAMPLE10("samples_5_min.csv"), SAMPLE11("samples_6.csv"), SAMPLE12("samples_7.csv"), SAMPLE13("samples_7.csv"),
+    SAMPLE14("samples_8.csv"), SAMPLE15("samples_9.csv"), SAMPLE16("samples_10.csv");
 
 
     private final String fileName;
